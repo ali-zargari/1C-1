24d2edd0b5c2aea9656657450ec93b888b6b1b49
Ali Zargari <77089778+ali-zargari@users.noreply.github.com>
2021-11-02 14:30:19 -0700

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,20 @@
+project folder:
+ali-zargari-cs1c-project01/
+
+
+Brief description of submitted files:
+
+src/DemoGit.java
+    - Outputs the author's name.
+
+resources/proof_of_execution.mp4
+    - Video proof that the program runs.
+
+resources/proof_pic.png
+    - Picture proof that the program runs.
+
+resources/RUN.txt
+    - console output of DemoGit.
+
+README.txt
+    - description of submitted files
---
+++ src/DemoGit.java
@@ -0,0 +1,24 @@
+/**
+ * Simple class to test submitting to GitHub repository.
+ * @author Foothill College, Ali Zargari
+ *
+ * Note: If you modified this file then update the author name with your: FirstName LastName
+ * 
+ * Reminder: Take a look at "Example Program Submission" in course Modules and follow the
+ *           "Program Guidelines" to make sure your submission is complete.
+ */
+public class DemoGit 
+{
+
+	/**
+	 * Outputs my name
+	 *
+	 * @param args not used.
+	 */
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello. My name is Ali Zargari");
+
+	}
+
+}
